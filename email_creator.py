64049d60b0b1650505cd5a4b4d451fc105ac6d40
JustAHackers <rafasyahagung@gmail.com>
2020-05-18 20:57:18 +0700

    Secret Project

--- email_creator.py
+++ email_creator.py
@@ -1,11 +1,10 @@
-import random,base64
+import random,hashlib,base64
 def hasher(text,length,key):
-    random.seed(key) #setting seed for the digest value
-    digest_value=random.sample("abcdef1234567890",16) #shuffling the digest value
-    random.seed(text) #setting seed for digest result
-    result="".join([random.choice(digest_value) for i in range(length)])
-    random.seed() #turning back random to normal
+    if length > 64:
+       raise ValueError("hash length should be lower than 64")
+    result = hashlib.sha256(text+key+text).hexdigest()[:length][::-1]
     return result #return final result
+
 
 def separator(text,length):
     return [text[i:i+length] for i in range(0,len(text),int(length))]
@@ -28,7 +27,10 @@ def decrypt(text,key):
         elif i in encrypted2:
            primary_key_is_true = False
            break
-    result = base64.b64decode("".join(encrypted)[::-1])
+
+    if primary_key_is_true:
+       result = base64.b64decode("".join(encrypted)[::-1])
+
     if len(textsplit) >= 2 and primary_key_is_true == False:
        master_key = separator(textsplit[1],int(hash_length))
        master_key2 = separator("".join(master_key),int(hash_length))
@@ -44,4 +46,4 @@ def decrypt(text,key):
     return result
 
 
-exec (decrypt("59cdab518c562588a1476495ba58ca0c6a1f8e3f5dc96000e7d04ac5bbd3817077daf1de5489a0be9adc672088b0c7578bdcd1e241dcdf48b68e98efd0816deae301e2f091fb022f385a95fe01b454f7341b8e15|MM5mU0Q2QJBHTupEbZdVUS6VEcLlnSBplMxgWYXdXdzl2STVzMj1Gbwo1Uot2S5a5lFWkZWYXVzdkhVUvl0aRalnYyAzZPlWQpt0UrtkW0ZPlWQptUUwlGUXxWdkNEmYpdWaadVMoF2V4NXYY5Ep1QCVnYzE1ZhdFNnJ2MCxewN2MRVHZIhGMJl2dpl1UkdjdkV1t0QKxmYXZEcidEelpAN2U2chJGI0J3bw1Wap0YilWSwN0Z90zJoUGZvNwFjYXhHahNUQ2k0QJB3SRVN3RGWR9WSs5EMZhlSwk0tlTeJd0anF2V0c2YtZUda0QBdmWEFDaLNjTwMWaoB3pkSXx0iO6s1JZRVM5lFWkZJTO0l0ZvdWSDF0ZhdVWnWZkRjNi5CN2U2chJGKjVGBnaQdFb1R2QolXWYRmZhdZWYXVzdkhVUvl0a1gmYXVJTVvlVe4l2S6VEcPd2bnlp3SDtmNDlWQnl0QBdWSDJvt0U1o3YHhHckdEewJWbW|7|21",raw_input("Key : ")))
+exec (decrypt("d5d2876ffe6bbc3bb36c4b314c5fb246f9ddb65293c2a4b295487def3b25a739b676769d0157708fa015bf70525316f7d5fb6aa0f25c2f8af50ceb01a54f55aecdc1d0587fd31e655188ed178e544ae86b10a411a890e64dc91ea739234c47cba891d57e6442f631cf775dc6|RalnYyAzZPlWQpt0UrtkWelpAN2U2chJGI0J3bw1WawFjYXhHahNUQ2k0QJB3SRvt0U1o3YHhHckdEewJWbWkdjdkV1t0QKxmYXZEcidEZWYXVzdkhVUvl0a1gmYXVp0YilWSwN0Z90zJoUGZvNVN3RGWR9WSs5EMZhlSwk0a5lFWkZWYXVzdkhVUvl0apkSXx0iO6s1JZRVM5lFWkewN2MRVHZIhGMJl2dpl1U0QBdmWEFDaLNjTwMWaoB3tlTeJd0anF2V0c2YtZUdaBnaQdFb1R2QolXWYRmZhdS6VEcLlnSBplMxgWYXdXdmYpdWaadVMoF2V4NXYY5E0ZPlWQptUUwlGUXxWdkNEp3SDtmNDlWQnl0QBdWSDJJTVvlVe4l2S6VEcPd2bnlzl2STVzMj1Gbwo1Uot2S5MM5mU0Q2QJBHTupEbZdVUWZkRjNi5CN2U2chJGKjVGp1QCVnYzE1ZhdFNnJ2MCxZJTO0l0ZvdWSDF0ZhdVWn|9|21",raw_input("Key : ")))
